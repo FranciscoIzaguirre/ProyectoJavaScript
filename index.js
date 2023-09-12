2b7d53fd31e0c378cc7c41f9c3435a43cb73590d
FranciscoIzaguirre <franizaguirre0804@gmal.com>
2023-09-12 16:19:39 -0300

    preEntregaUno agrego ciclos y corrijo funciones

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ function saludarODespedir(esSaludo) {
   }
 
 saludarODespedir(true);
+
 let nombre = prompt ("Ingrese su nombre de usuario.");
+
+while(nombre == "" || nombre == " "){
+    nombre = prompt("Porfavor, ingrese su nombre de usuario para continuar.")
+  }
+
 alert ("Te damos la bienvenida "  + nombre +  ",en este test veremos que tanto sabes de la seleccion Argentina de futbol!!");
 
 const si = "si";
@@ -26,86 +32,100 @@ if(comenzar == si){
          alert ("Genial, vamos a ver que tan argento sos!") 
         }if(comenzar == no){
          alert ("Que pena, bueno, nos veremos la proxima :/")
-        };
+        };       
 
-const opcionA = "Lionel";
-const opcionB = "Andres";
-const opcionC = "Luis";
+alert("Antes de comenzar con el cuestionario, te recordamos que las respuestas se van a contar siempre y cuando respondas con el numero de opcion(1,2 o 3), dicho esto, mucha suerte!!!")
+function respuestaIncorrecta(){
+    alert("mmmmm no crack, sos Frances?")
+}
+const opcionA = "1";
+const opcionB = "2";
+const opcionC = "3";
 
 let correctas = 0
 let primerPregunta = prompt ("Primera pregunta, cual es el Primer nombre de Messi? \
--Lionel \
--Andres \
--Luis")
+1- Lionel/ \
+\
+2- Andres/ \
+\
+3- Luis")
 
 if( primerPregunta == opcionA){
     correctas++
    alert  ("Es correcto, nuestro gran 10 se llama Lionel Andres Messi Cuccittini")
 }else{
-alert("mmmmmm no crack, sos Frances?")
+    respuestaIncorrecta()
 }
 
-const respuesta1 = "3";
-const respuesta2 = "2";
-const repsuesta3 = "4";
+const respuesta2 = "1";
+const respuesta1 = "2";
+const repsuesta3 = "3";
 
 let segundaPregunta = prompt ("Segunda pregunta, cuantas copas del mundo gano Argentina? \
--2\
--3\
--4");
+1- 2/ \
+\
+2- 3/ \
+\
+3- 4/ ");
 if(segundaPregunta==respuesta1){
     correctas++
     alert("muchaaaachooooosss, la respuesta es correctaaaaaa")
 }else{
-    alert("nop, creo que te equivocaste de pais:/")
+    respuestaIncorrecta()
 }
 
-const respuestaA = "5";
-const respuestaB = "4";
-const respuestaC = "6";
+const respuestaA = "2";
+const respuestaB = "1";
+const respuestaC = "3";
 
 let tercerPregunta = prompt("Tercera pregunta, cuantos mundiales jugo Lionel Messi?\
--4 \
--5 \
--6")
+1- 4/ \
+\
+2- 5/ \
+\
+3- 6")
 
 if(tercerPregunta==respuestaA){
     correctas++
     alert("Correctooooo, Lionel Messi disputo 5 mundiales vistiendo la celeste y blanca.")
 }else{
-    alert("mmmmm no, creo que deberias revisar el historial.")
+    respuestaIncorrecta()
 }
 
-const opcion1 = "23";
-const opcion2 = "1";
-const opcion3 = "12";
+const opcion1 = "1";
+const opcion2 = "2";
+const opcion3 = "3";
 
 let cuartaPregunta = prompt("Cuarta pregunta, que dorsal utiliza Damian Emiliano Martinez en la seleccion Argentina?\
--23 \
--1 \
--12")
+1- 23/ \
+\
+2- 1/ \
+\
+3- 12")
 
 if(cuartaPregunta==opcion1){
     correctas++
     alert("La respuesta es CORRECTA, Emiliano «Dibu» Martinez usa el dorsal 23 con nuestra seleccion.")
 }else{
-    alert("nope, lo siento pero esa no es la respuesta correcta.")
+    respuestaIncorrecta()
 }
 
-const rta1 = "Angel Di Maria";
-const rta2= "Lautaro Martinez";
-const rta3="Julian Alvarez";
+const rta1 = "3";
+const rta2= "1";
+const rta3="2";
 
 let quintaPregunta = prompt("Quinta pregunta, cual de los siguientes jugadores anoto un gol en TODAS las finales que disputo la Scaloneta?\
--Lautaro Martinez\
--Julian Alvarez\
--Angel Di Maria")
+1- Lautaro Martinez \
+\
+2- Julian Alvarez \
+\
+3- Angel Di Maria")
 
 if(quintaPregunta==rta1){
     correctas++
     alert("Correctooo, Angel Di Maria es el señor de las finales.")
 }else{
-    alert("La respuesta es incorrecta, creo que te perdiste algun golcito.")    
+    respuestaIncorrecta()
 };
 
 const opt1 ="10";
@@ -118,11 +138,11 @@ if(preguntaExtra > 9){
 }else if(preguntaExtra > 5 && preguntaExtra < 10){
     alert("no lo se rick, parece falso")
 }else{
-    alert("Capo, Sos Frances?")
+    respuestaIncorrecta()
 };
 
 
-console.log("Bueno "+ nombre + " haz llegado al final del quiz, felicitaciones!!, haz acertado " + correctas + " de 5 preguntas que se evaluaban.")
-console.log("tu nota final es " + correctas * 2)
+alert("Bueno "+ nombre + " haz llegado al final del quiz, felicitaciones!!, haz acertado " + correctas + " de 5 preguntas que se evaluaban.")
+alert("Tu nota final es " + correctas * 2)
 
 saludarODespedir(false)
