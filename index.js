8915b1e7f791ea980890392cdc3f975402a0c6ee
FranciscoIzaguirre <franizaguirre0804@gmail.com>
2023-10-24 17:43:26 -0300

    agregando sweet alert y storage con falta de preguntasd

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-
+const btnInicio = document.getElementById("btnInicio");
+const reiniciarButton = document.getElementById("reiniciar");
 const nombreInput = document.getElementById("nombre");
 const preguntasDiv = document.getElementById("preguntas");
 const mensajeP = document.getElementById("mensaje");
 const resultadoP = document.getElementById("resultado");
-const respuestas = [1, 2, 2, 1, 3, 2, 1, 1, 3, 2]; // Respuestas correctas
+const opcionesButtons = preguntasDiv.querySelectorAll("button");
 
-localStorage.setItem("respuestas", JSON.stringify(respuestas));
+btnInicio.addEventListener("click", iniciarQuiz);
+
+opcionesButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        const opcion = button.getAttribute("data-opcion");
+        responder(parseInt(opcion)); 
+    });
+});
+
+reiniciarButton.addEventListener("click", reiniciarCuestionario);
 
 let nombre = "";
 let preguntaActual = 0;
@@ -14,19 +24,41 @@ let respuestasCorrectas = 0;
 let boton = document.getElementById("btnInicio")
 boton.addEventListener("click", respuestaClick)
 function respuestaClick() {
-    alert("Bienvenido " + nombre + ",te deseamos muchisima suerte en el quiz, vamos a ver que tan Argento sos!!!")
+    Swal.fire({
+        title: "Bienvenido " + nombre + ",te deseamos muchisima suerte en el quiz, vamos a ver que tan Argento sos!!!",
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 2000
+    })
 }
-
 function iniciarQuiz() {
-    nombre = nombreInput.value;
-    if (nombre) {
-        nombreInput.style.display = "none";
-        preguntasDiv.style.display = "block";
-        mostrarSiguientePregunta();
+    const preguntasJSON = localStorage.getItem("preguntas");
+    const preguntas = JSON.parse(preguntasJSON);
+
+    if (preguntas) {
+        nombre = nombreInput.value;
+        if (nombre) {
+            nombreInput.style.display = "none";
+            preguntasDiv.style.display = "block";
+            mostrarSiguientePregunta();
+        }
+    } else {
+        console.error("No se pudieron cargar las preguntas desde el localStorage.");
     }
+
+    fetch("preguntas.json")
+    .then((response) => response.json())
+    .then((data) => {
+        localStorage.setItem("preguntas", JSON.stringify(data));
+    })
+    .catch((error) => {
+        console.error("Error al obtener las preguntas:", error);
+    });
 }
 
 function mostrarSiguientePregunta() {
+    const preguntas = JSON.parse(localStorage.getItem("preguntas"));
+
     if (preguntaActual < preguntas.length) {
         mensajeP.textContent = preguntas[preguntaActual].pregunta;
         const opciones = preguntas[preguntaActual].opciones;
@@ -42,7 +74,19 @@ function mostrarSiguientePregunta() {
 function responder(opcion) {
     if (opcion === respuestas[preguntaActual]) {
         respuestasCorrectas++;
+        Swal.fire({
+            title: "Respuesta Correcta",
+            text: "MUCHAAACHOOOSSS!!!",
+            icon: "success",
+        });
+    } else {
+        Swal.fire({
+            title: "Respuesta Incorrecta",
+            text: "mmm, sos Frances?",
+            icon: "error",
+        });
     }
+
     preguntaActual++;
     mostrarSiguientePregunta();
 }
@@ -50,46 +94,23 @@ function responder(opcion) {
 function mostrarResultado() {
     preguntasDiv.style.display = "none";
     resultadoP.style.display = "block";
-    resultadoP.textContent = "felicitaciones!!!" + nombre + " , haz acertado " + respuestasCorrectas + " de " + preguntas.length + " preguntas.";
-
+    resultadoP.textContent = "Felicitaciones, " + nombre + ", haz acertado " + respuestasCorrectas + " de " + preguntas.length + " preguntas.";
 }
-
-const preguntas = [
-    { pregunta: "Primera pregunta, cual es el Primer nombre de Messi?", opciones: ["Lionel", "Andres", "Luis"] },
-    { pregunta: "Segunda pregunta, cuantas copas del mundo gano Argentina?", opciones: ["2", "3", "4"] },
-    { pregunta: "Tercera pregunta, cuantos mundiales jugo Lionel Messi?", opciones: ["4", "5", "6"] },
-    { pregunta: "Cuarta pregunta, que dorsal utiliza Damian Emiliano Martinez en la seleccion Argentina?", opciones: ["23", "1", "12"] },
-    { pregunta: "Quinta pregunta, cual de los siguientes jugadores anoto un gol en TODAS las finales que disputo la Scaloneta?", opciones: ["Lautaro Martinez", "Julian Alvarez", "Angel Di Maria"] },
-    { pregunta: "Sexta pregunta, Cual fue el mayor numero de partidos invictos que mantuvo el conjunto de Scaloni?", opciones: ["30", "36", "40"] },
-    { pregunta: "Septima pregunta, a que pais le gano Argentina en la final de la copa America?", opciones: ["Brasil", "Uruguay", "Venezuela"] },
-    { pregunta: "Octava pregunta, Quien es el maximo goleador de la Era Scaloni?", opciones: ["Lionel Messi", "Lautaro Martinez", "Angel Di Maria"] },
-    { pregunta: "Novena pregunta, Cuantos titulos tiene Lionel Messi con la Celeste y blanca? ", opciones: ["3", "6", "5"] },
-    { pregunta: "Decima pregunta, esta es para verdaderos aficionados, Ante el partido de eliminatorias contra Paraguay, Emiliano Martinez rompio un record, El arquero del combinado nacional se convirtió en el Arquero con mayor tiempo sin que le conviertan goles, cuanto tiempo fue? ", opciones: ["550 minutos", "622 minutos", "300 minutos"] },
-];
 
 function reiniciarCuestionario() {
     nombre = "";
     preguntaActual = 0;
     respuestasCorrectas = 0;
     
-    const reiniciarButton = document.getElementById("reiniciar");
     reiniciarButton.style.display = "none";
-
-    
     nombreInput.style.display = "block";
     preguntasDiv.style.display = "none";
-    resultadoP.style.display = "none";
-    resultadoOpinionP.style.display = "none";
 
-    
     nombreInput.value = "";
     mensajeP.textContent = "";
     resultadoP.textContent = "";
-    opinionInput.value = "";
-    resultadoOpinionP.textContent = "";
 
-
-    iniciarButton.style.display = "block";
+    btnInicio.style.display = "block";
 }
 
 
